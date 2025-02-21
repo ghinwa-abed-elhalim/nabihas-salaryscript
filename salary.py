501730f7b4b92cc8ee7c1642c47064ea73ae5769
ghinwa-abed-elhalim <ghenwaah77@gmail.com>
2025-02-21 00:46:53 +0000

    variable percentages added

--- salary.py
+++ salary.py
@@ -1,6 +1,12 @@
-print("hello nabiha this is your salary script")
+print("hello nabiha this is your salary saving script")
 
-salary = (int (input("what is your monthly income? $")))
+salary = (float(input("what is your monthly income? $")))
 
 month = input("what is the month you are storing the salary for? ")
+
+per_savings, per_rent, per_electricity = input("enter the percentage of: savings, rent, and electricity separated by space. ").split()
+
+print(f"savings percentage: {per_savings}")
+print(f"rent percentage: {per_rent}")
+print(f"electricity percentage: {per_electricity}")
 
