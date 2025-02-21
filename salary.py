b05218460ccac287cf7ee506afa14e442e34fbd1
ghinwa-abed-elhalim <ghenwaah77@gmail.com>
2025-02-21 01:30:33 +0000

    edited first two variables

--- salary.py
+++ salary.py
@@ -1,8 +1,8 @@
 print("hello nabiha this is your salary saving script")
 
-salary = (float(input("what is your monthly income? $")))
+salary = float(input("what is your monthly income? $"))
 
-month = input("what is the month you are storing the salary for? ")
+month = input("what is the month you are storing the salary for ? ")
 
 per_savings, per_rent, per_electricity = input("enter the percentage of: savings, rent, and electricity separated by space. ").split()
 
